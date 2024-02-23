4b4955d59225872b4a5be4384baa579993dad3a0
Divyansh Mehta <145423867+dev4thedeveloper@users.noreply.github.com>
2024-02-23 15:49:57 +0530

    api key updated....

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: `${process.env.RAWG_API}`,
+    key: "6fdf47f0da2f48d086b485ff64abb795",
   },
 });
